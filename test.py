37f86110ae78f9cdadfadf18a0e66929775dd1aa
Bibekpandey <spirit_bibek@yahoo.com>
2018-02-01 22:54:58 +0000

    add two circles in same frame, basic one

--- test.py
+++ test.py
@@ -6,11 +6,20 @@ def test_animator():
     conf.duration = 1
     conf.fps = 30
     animator = Animator(conf)
-    circle =  Circle(CircleConfig())
-    #animator.add_frame_object(0, circle)  # set first frame
-    #animator.add_frame_object(0, circle.translate((30, 400)))  # add another circle
+    circ_conf = CircleConfig()
+    circle = Circle(circ_conf)
+    # animator.add_frame_object(0, circle)  # set first frame
+    # animator.add_frame_object(0, circle.translate((30, 400)))  # add another circle
     translated_objects = circle.translate((100, 300), frames=animator.total_frames)
     animator.add_frames_objects(0, translated_objects)
+
+    # another circle
+    circ_conf.color = (0,0,255,255)
+    circ_conf.center = (100, 20)
+    blue = Circle(circ_conf)
+    blue_translated = blue.translate((400, 400), frames=animator.total_frames)
+    animator.add_frames_objects(0, blue_translated)
+
     animator.compile_frames()
     images = animator.get_compiled_frames()
     for i, img in enumerate(images):
